b0dbeb09935b4ca2f10bc8c68a8900539af35444
Yuanjian Liu <achievinglegend@gmail.com>
2023-05-04 16:14:10 -0500

    fix favicon

--- js/script.js
+++ js/script.js
@@ -34,9 +34,9 @@ const allLinks = document.querySelectorAll("a:link");
 
 allLinks.forEach(function (link) {
   link.addEventListener("click", async (e) => {
-    e.preventDefault();
+    
     const href = link.getAttribute("href");
-    console.log(href);
+    e.preventDefault();
     // Scroll back to top
     if (href === "#") {
       window.scrollTo({
